155ada897b81992283fe97745298f810789847c5
Manish <[manishbulchandani4@gmail.com]>
2025-01-12 23:35:30 +0530

    Docker remove

--- wp-config.php
+++
@@ -1,5 +0,0 @@
-define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
-define('DB_USER', getenv('WORDPRESS_DB_USER'));
-define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
-define('DB_HOST', getenv('WORDPRESS_DB_HOST') . ':' . getenv('WORDPRESS_DB_PORT'));
-
